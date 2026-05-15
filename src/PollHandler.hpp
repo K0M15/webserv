f2f095def9cd22a97585e33134f7da729b548b94
Alain Felger(afelger) <alain.felger93@gmail.com>
2026-05-15 14:53:44 +0200

    start of pollhandler, issues with some parts of code still persist

--- src/PollHandler.hpp
+++ src/PollHandler.hpp
@@ -1,40 +1,115 @@
 #include <functional>
 #include <map>
 #include <vector>
+#include <algorithm>
 #include <poll.h>
 #include <HttpServerException.hpp>
 
+typedef std::function<void()> fvoid_t;
+
 class PollHandler {
 private:
+    int timeout;
     struct EventType{
         int fd;
         short event;
-        std::function<void()> cb;
+        fvoid_t on_readable;
+        fvoid_t on_writeable;
+        fvoid_t on_close; // triggers on POLLERR, POLLRDHUP or POLLNVAL
     };
     std::vector<struct EventType> events;
-    int timeout;
 public:
-    PollHandler() : timeout(300), events() {}
+    PollHandler() : timeout(3000), events() {}
     PollHandler(int t) : timeout(t), events(){}
-    PollHandler(PollHandler& other);
-    PollHandler& operator=(PollHandler& other);
-    ~PollHandler();
+    PollHandler(PollHandler& other): timeout(other.timeout), events(other.events){}
+    PollHandler& operator=(PollHandler& other){
+        if (&other != this)
+        {
+            timeout = other.timeout;
+            events = other.events;
+        }
+        return *this;
+    }
+    ~PollHandler() = default;
 
-    void subscribe(int fd,  std::function<void()> handler);
-    void unsubscribe(int fd);
+    struct EventType* getEventByFD(int fd){
+        for(auto& e: events)
+        {
+            if (e.fd == fd){
+                return &e;
+            }
+        }
+        return nullptr;
+    }
+    void subscribe_read(int fd, fvoid_t on_close, fvoid_t on_readable){
+        subscribe(fd, on_close, on_readable, nullptr);
+    }
+    void subscribe_write(int fd, fvoid_t on_close, fvoid_t on_writeable){
+        subscribe(fd, on_close, nullptr, on_writeable);
+    }
+    void subscribe(int fd, fvoid_t on_close, fvoid_t on_readable, fvoid_t on_writeable){
+        EventType d;
+        d.event = ( on_close == nullptr ? 0 : POLLERR | on_readable == nullptr ? 0 : POLLIN | on_writeable == nullptr ? 0 : POLLOUT );
+        if (d.event == 0) throw HttpServerException("No eventlistener is provided");
+        d.fd = fd;
+        d.on_close = on_close;
+        d.on_readable = on_readable;
+        d.on_writeable = on_writeable;
+        auto e = getEventByFD(fd);
+        if (e != nullptr){
+            e->event = d.event;
+            e->fd = d.fd;
+            e->on_close = d.on_close;
+            e->on_readable = d.on_readable;
+            e->on_writeable = d.on_writeable;
+            return;
+        }
+        events.push_back(d);
+    }
+
+    void unsubscribe(int fd) {
+        events.erase(
+            std::remove_if(events.begin(), events.end(), [fd](const struct EventType& e) {
+                return e.fd == fd;
+            }), 
+            events.end()
+        );
+    };
 
     void checkFDs() {
-        struct pollfd* check = (struct pollfd*)calloc(events.size(), sizeof(struct pollfd));
-        int i = 0;
-        for (std::vector<struct EventType>::iterator e = events.begin(); e != events.end(); e++)
-        {
-            check[i].events = e->event;
-            check[i].fd = e->fd;
-            i++;
+        if (events.empty()) {
+            return;
         }
-        int result = poll(check, i, timeout);
-        if (result < 0)
+        std::vector<struct pollfd> pollfds;
+        pollfds.reserve(events.size());
+
+        for (const auto& e : events) {
+            struct pollfd pfd;
+            pfd.fd = e.fd;
+            pfd.events = e.event;
+            pfd.revents = 0;
+            pollfds.push_back(pfd);
+        }
+        int result = poll(pollfds.data(), pollfds.size(), timeout);
+        if (result < 0) {
             throw HttpServerException("Error on poll");
-        // check each result, if revents has been set, then call cbs if appropriate event is active
+        }
+        
+        if (result == 0) {
+            return;
+        }
+
+        for (size_t i = 0; i < pollfds.size(); ++i) {
+            short revents = pollfds[i].revents;
+            if (revents == 0) continue;
+            if (revents & (POLLERR | POLLHUP | POLLNVAL)) {
+                if (events[i].on_close) events[i].on_close(); // what if element is removed?
+                continue;
+            }
+            if (revents & (POLLIN | POLLPRI))
+                if (events[i].on_readable) events[i].on_readable(); // what if element is removed?
+            if (revents & POLLOUT)
+                if (events[i].on_writeable) events[i].on_writeable();
+        }
     }
 };
