eaed2cdf8f4bb6786c81bad10724d51213d04ba3
Alain Felger <alain.felger93+git@gmail.com>
2026-08-23 19:18:24 +0200

    changed URL parser: % encoding # error space checking clear readable code

--- include/URL.hpp
+++ include/URL.hpp
@@ -2,71 +2,24 @@
 
 #include <string>
 #include <unordered_map>
-#include <regex>
+#include <iostream>
 #include "HttpServerException.hpp"
+#include "PathUtils.hpp"
 
-class URL{
+class URL {
 public:
-    URL() : value() {}
-    explicit URL(const std::string& s) : value(std::move(s)){
-        // allow absolute URI, absolute path (origin-form), or asterisk (*)
-        const std::regex url_pattern( //static const for more performance ?
-        R"(^(?!.*\/\.\.?\/)((https?|ftp)://[^\s/$.?#].[^\s]*|/[^\s]*|\*)$)",
-        std::regex::icase);
-        if (!std::regex_match(value, url_pattern))
-        {
-            throw HttpServerException("URL String does not match any valid request-target pattern");
-        }
-    }
-    const std::string str() const { return value;}
-    void setURL(const std::string& s){
-        URL url(s); //checks data
-        this->value = std::move(s); // replaces current value
-    }
-    const std::string getRawQuery() const
-    {
-        size_t separator = value.find('?');
-        if (separator == value.npos)
-            return "";
-        return value.substr(separator + 1);
-    }
+    URL();
+    explicit URL(const std::string& s);
     
-    const std::string getFileExt() const
-    {
-        std::string path = value.substr(0, value.find('?'));
-        size_t dot = path.rfind('.');
-        size_t slash = path.rfind('/');
-        if (dot == std::string::npos || (slash != std::string::npos && dot < slash))
-            return "";
-        return path.substr(dot);
-    }
+    const std::string str() const;
+    void setURL(const std::string& s);
+    const std::string getRawQuery() const;
+    const std::string getFileExt() const;
+    const std::unordered_map<std::string, std::string> getQuery() const;
 
-    const std::unordered_map<std::string, std::string> getQuery() const
-    {
-        std::unordered_map<std::string, std::string> result;
-        std::string raw = getRawQuery();
-        size_t pos = 0;
-        while (pos < raw.size())
-        {
-            size_t next_and = raw.find("&", pos);
-            std::string curr_pair = (next_and == raw.npos ? raw.substr(pos) : raw.substr(pos, next_and - pos));
-            size_t curr_equ = curr_pair.find('=');
-            if (curr_equ != curr_pair.npos)
-                result[curr_pair.substr(0, curr_equ)] = curr_pair.substr(curr_equ + 1);
-            else if (curr_pair.length() != 0)
-                result[curr_pair] = "";
-            if (next_and == raw.npos) break;
-            pos = next_and + 1;
-        }
-        return result;
-    }
 private:
+    static bool isValidURL(const std::string& url);
     std::string value;
 };
 
-inline std::istream& operator>>(std::istream& is, URL& url){
-    std::string temp;
-    if (is >> temp)
-        url.setURL(temp);
-    return is;
-}
+std::istream& operator>>(std::istream& is, URL& url);
