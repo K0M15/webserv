b4a691e11942fca84c9f6b118c6b9b994c9021cd
Alain Felger <alain.felger93+git@gmail.com>
2026-08-25 20:23:19 +0200

    removed tests

--- www/test/cgi/python/echo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-import sys
-
-body = sys.stdin.read()
-sys.stdout.write("Content-Type: text/plain\r\n\r\n")
-sys.stdout.write("QUERY=%s\n" % os.environ.get("QUERY_STRING", ""))
-sys.stdout.write("BODY=%s\n" % body)
-sys.stdout.write("TE=%s\n" % os.environ.get("HTTP_TRANSFER_ENCODING", ""))
--- www/test/cgi/python/hello.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-sys.stdout.write("Content-Type: text/plain\r\n\r\n")
-sys.stdout.write("Hello from Python CGI!\n")
